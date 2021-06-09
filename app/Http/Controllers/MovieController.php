9e67f849e2b5aeadef56e6afe9255eea85b52266
danibr1 <danielebr1987@gmail.com>
2021-06-09 19:33:57 +0200

    print from db

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+// USE APP MOVIE
+use App\Movie;
+
+use Illuminate\Http\Request;
+
+class MovieController extends Controller
+{
+    public function index() {
+        $movies = Movie::all();
+        dump($movies);
+
+        return view('movies', compact('movies'));
+    }
+}
